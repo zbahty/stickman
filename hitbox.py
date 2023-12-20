214766b678cc16de01e8210909d9bf8dbc113cb9
zbahty <b.akguen@edu.letteverein.berlin>
2023-12-20 12:31:26 +0100

    Revert "init commit"
    This reverts commit e3790d49fef867e9f96a7fbbd795bf6867c69b78.

--- hitbox.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class hitbox():
-    def __init__(self,x,y):
-        self.rect = pygame.Rect((x,y,80,180))
-
-    def draw(self,surface):
-        pygame.draw.rect(surface,(255,0,0),self.rect)
-  
